21ad900b9af17bdaf6cf83ae2c7ecdbabf6034ea
Jondy Zhao <jondy.zhao@gmail.com>
2020-07-15 16:18:12 +0800

    Add new platforms

--- platforms/download.sh
+++ platforms/download.sh
@@ -6,6 +6,8 @@ RPATH="jondy@192.168.121.103:/home/jondy/workspace"
 echo "Download remote files from $RPATH"
 
 set -x
+set -e
+
 scp -i $IDFILE $RPATH/pyarmor-core/platforms/win_amd64/_pytransform.dll ./win_amd64
 scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux_x86_64/_pytransform.so ./linux_x86_64
 scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux_i386/_pytransform.so ./linux_i386
@@ -35,6 +37,8 @@ scp -i $IDFILE $RPATH/pytransform/cross-platform/linux.armv6/.libs/_pytransform.
 scp -i $IDFILE $RPATH/pytransform/cross-platform/android.armv7/.libs/_pytransform.so ./android.armv7.0
 scp -i $IDFILE $RPATH/pytransform/cross-platform/uclibc.armv7/.libs/_pytransform.so ./uclibc.armv7.0
 scp -i $IDFILE $RPATH/pytransform/cross-platform/musl.mips32/.libs/_pytransform.so ./musl.mips32.0
+scp -i $IDFILE $RPATH/pytransform/cross-platform/linux.mips64/.libs/_pytransform.so ./linux.mips64.0
+scp -i $IDFILE $RPATH/pytransform/cross-platform/linux.mips64el/.libs/_pytransform.so ./linux.mips64el.0
 
 for v in 27 37 38 ; do
     scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.x86_64.11.py${v}/pytransform*.so \
@@ -60,3 +64,14 @@ else
     scp -i $IDFILE $RPATH/pyarmor-core/platforms/darwin.x86_64.0/_pytransform.dylib ./darwin.x86_64.0
     scp -i $IDFILE $RPATH/pyarmor-core/platforms/ios.arm64/_pytransform.dylib ./ios.arm64
 fi
+
+# Copy vm protection libraries
+scp -i $IDFILE $RPATH/pyarmor-core/platforms/windows.x86_64.21/_pytransform.dll ./windows.x86_64.21
+scp -i $IDFILE $RPATH/pyarmor-core/platforms/windows.x86.21/_pytransform.dll ./windows.x86.21
+
+for v in 27 37 38 ; do
+    scp -i $IDFILE $RPATH/pyarmor-core/platforms/windows.x86_64.25.py${v}/pytransform.pyd \
+        ./windows.x86_64.25.py${v}
+    scp -i $IDFILE $RPATH/pyarmor-core/platforms/windows.x86.25.py${v}/pytransform.pyd \
+        ./windows.x86.25.py${v}
+done
