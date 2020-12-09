d848084fdd70a43dda285e06f4b64a5f73cf9e96
Jondy Zhao <jondy.zhao@gmail.com>
2020-12-09 14:08:06 +0800

    Update all to r38.12

--- platforms/download.sh
+++ platforms/download.sh
@@ -64,6 +64,14 @@ scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.x86_64.11.py27ucs4/pytransfor
     ./linux.x86_64.11.py27
 scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.x86.11.py27ucs4/pytransform*.so \
     ./linux.x86.11.py27
+scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.aarch64.11.py27ucs4/pytransform*.so \
+    ./linux.aarch64.11.py27
+scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.aarch32.11.py27ucs4/pytransform*.so \
+    ./linux.aarch32.11.py27
+scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.armv7.11.py27ucs4/pytransform*.so \
+    ./linux.armv7.11.py27
+scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.armv6.11.py27ucs4/pytransform*.so \
+    ./linux.armv6.11.py27
 scp -i $IDFILE $RPATH/pyarmor-core/platforms/linux.x86_64.11.py27/pytransform*.so \
     ./centos6.x86_64.11.py27
 
